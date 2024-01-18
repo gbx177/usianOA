395243f4a9f0531c91002216644520551234dff8
gbx <3180389867@qq.com>
2024-01-18 17:07:05 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '积云人力资源管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon iconClass="eye"></svg-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsianOAIndex',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
